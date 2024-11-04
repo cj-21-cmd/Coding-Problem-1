4d3d38be602143449c6a2364b5fbcc12de693836
cjdelacruz <cjdelacruz0521@gmail.com>
2024-11-04 17:30:01 +0700

    Initial commit with userProfile.s

---
+++ userProfile.js
@@ -0,0 +1,13 @@
+//Collect the following user information:
+
+let fullname = "Cassandra Jade H. Delacruz";
+console.log("Full Name:", fullname);
+
+let age = "19";
+console.log("Age:", age);
+
+let favNumber = "21";
+console.log("Favorite Number:", favNumber);
+
+let favColor = "White";
+console.log("Favorite Color:", favColor);
